03ad2e702a1c938391f5eae72be534a9a2c6db8b
bamaham93 <bamaham93@gmail.com>
2022-07-31 22:39:35 -0400

    Finished signup and login process.

--- flight_management/accounts/tests.py
+++ flight_management/accounts/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
 # Create your tests here.
+class TestAccounts(TestCase):
+    """
+    """
+
+    def test_this(self):
+        """
+        """
+        assert(True, True)
--- flight_management/flight_management/urls.py
+++ flight_management/flight_management/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('accounts.urls')),
     path('aircraft/', include('accounts.urls')),
 ]
